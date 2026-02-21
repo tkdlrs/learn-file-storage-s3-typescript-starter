d1de44a9d22691f9756d785a58f7bf23bca86df6
tkdlrs <tkd.lsanchez@gmail.com>
2026-02-20 19:10:21 -0700

    Completes 'Chapter 2: Caching | Lesson 3: Cache Busting' of the 'Learn File Servers...in TypeScript' course on Boot.dev.

--- src/app/app.js
+++ src/app/app.js
@@ -1,46 +1,55 @@
-document.addEventListener("DOMContentLoaded", async () => {
-  const token = localStorage.getItem("token");
+document.addEventListener('DOMContentLoaded', async () => {
+  const token = localStorage.getItem('token');
 
   if (token) {
-    document.getElementById("auth-section").style.display = "none";
-    document.getElementById("video-section").style.display = "block";
+    document.getElementById('auth-section').style.display =
+      'none';
+    document.getElementById('video-section').style.display =
+      'block';
     await getVideos();
   } else {
-    document.getElementById("auth-section").style.display = "block";
-    document.getElementById("video-section").style.display = "none";
+    document.getElementById('auth-section').style.display =
+      'block';
+    document.getElementById('video-section').style.display =
+      'none';
   }
 });
 
 document
-  .getElementById("video-draft-form")
-  .addEventListener("submit", async (event) => {
+  .getElementById('video-draft-form')
+  .addEventListener('submit', async (event) => {
     event.preventDefault();
     await createVideoDraft();
   });
 
 document
-  .getElementById("login-form")
-  .addEventListener("submit", async (event) => {
+  .getElementById('login-form')
+  .addEventListener('submit', async (event) => {
     event.preventDefault();
     await login();
   });
 
 async function createVideoDraft() {
-  const title = document.getElementById("video-title").value;
-  const description = document.getElementById("video-description").value;
+  const title =
+    document.getElementById('video-title').value;
+  const description = document.getElementById(
+    'video-description',
+  ).value;
 
   try {
-    const res = await fetch("/api/videos", {
-      method: "POST",
+    const res = await fetch('/api/videos', {
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${localStorage.getItem("token")}`,
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${localStorage.getItem('token')}`,
       },
       body: JSON.stringify({ title, description }),
     });
     const data = await res.json();
     if (!res.ok) {
-      throw new Error(`Failed to create video draft: ${data.error}`);
+      throw new Error(
+        `Failed to create video draft: ${data.error}`,
+      );
     }
 
     const videoID = data.id;
@@ -54,14 +63,15 @@ async function createVideoDraft() {
 }
 
 async function login() {
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
+  const email = document.getElementById('email').value;
+  const password =
+    document.getElementById('password').value;
 
   try {
-    const res = await fetch("/api/login", {
-      method: "POST",
+    const res = await fetch('/api/login', {
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify({ email, password }),
     });
@@ -71,12 +81,16 @@ async function login() {
     }
 
     if (data.token) {
-      localStorage.setItem("token", data.token);
-      document.getElementById("auth-section").style.display = "none";
-      document.getElementById("video-section").style.display = "block";
+      localStorage.setItem('token', data.token);
+      document.getElementById(
+        'auth-section',
+      ).style.display = 'none';
+      document.getElementById(
+        'video-section',
+      ).style.display = 'block';
       await getVideos();
     } else {
-      alert("Login failed. Please check your credentials.");
+      alert('Login failed. Please check your credentials.');
     }
   } catch (error) {
     alert(`Error: ${error.message}`);
@@ -84,22 +98,25 @@ async function login() {
 }
 
 async function signup() {
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
+  const email = document.getElementById('email').value;
+  const password =
+    document.getElementById('password').value;
 
   try {
-    const res = await fetch("/api/users", {
-      method: "POST",
+    const res = await fetch('/api/users', {
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify({ email, password }),
     });
     if (!res.ok) {
       const data = await res.json();
-      throw new Error(`Failed to create user: ${data.error}`);
+      throw new Error(
+        `Failed to create user: ${data.error}`,
+      );
     }
-    console.log("User created!");
+    console.log('User created!');
     await login();
   } catch (error) {
     alert(`Error: ${error.message}`);
@@ -107,47 +124,55 @@ async function signup() {
 }
 
 function logout() {
-  localStorage.removeItem("token");
-  document.getElementById("auth-section").style.display = "block";
-  document.getElementById("video-section").style.display = "none";
+  localStorage.removeItem('token');
+  document.getElementById('auth-section').style.display =
+    'block';
+  document.getElementById('video-section').style.display =
+    'none';
 }
 
 function setUploadButtonState(uploading, selector) {
   const uploadBtn = document.getElementById(selector);
   if (uploading) {
-    uploadBtn.textContent = "Uploading...";
+    uploadBtn.textContent = 'Uploading...';
     uploadBtn.disabled = true;
     return;
   }
-  uploadBtn.textContent = "Upload";
+  uploadBtn.textContent = 'Upload';
   uploadBtn.disabled = false;
 }
 
 async function uploadThumbnail(videoID) {
-  const thumbnailFile = document.getElementById("thumbnail").files[0];
+  const thumbnailFile =
+    document.getElementById('thumbnail').files[0];
   if (!thumbnailFile) return;
 
   const formData = new FormData();
-  formData.append("thumbnail", thumbnailFile);
+  formData.append('thumbnail', thumbnailFile);
 
-  const uploadBtnSelector = "upload-thumbnail-btn";
+  const uploadBtnSelector = 'upload-thumbnail-btn';
   setUploadButtonState(true, uploadBtnSelector);
 
   try {
-    const res = await fetch(`/api/thumbnail_upload/${videoID}`, {
-      method: "POST",
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem("token")}`,
+    const res = await fetch(
+      `/api/thumbnail_upload/${videoID}`,
+      {
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('token')}`,
+        },
+        body: formData,
       },
-      body: formData,
-    });
+    );
     if (!res.ok) {
       const data = await res.json();
-      throw new Error(`Failed to upload thumbnail. Error: ${data.error}`);
+      throw new Error(
+        `Failed to upload thumbnail. Error: ${data.error}`,
+      );
     }
 
     await res.json();
-    console.log("Thumbnail uploaded!");
+    console.log('Thumbnail uploaded!');
     await getVideo(videoID);
   } catch (error) {
     alert(`Error: ${error.message}`);
@@ -157,29 +182,35 @@ async function uploadThumbnail(videoID) {
 }
 
 async function uploadVideoFile(videoID) {
-  const videoFile = document.getElementById("video-file").files[0];
+  const videoFile =
+    document.getElementById('video-file').files[0];
   if (!videoFile) return;
 
   const formData = new FormData();
-  formData.append("video", videoFile);
+  formData.append('video', videoFile);
 
-  const uploadBtnSelector = "upload-video-btn";
+  const uploadBtnSelector = 'upload-video-btn';
   setUploadButtonState(true, uploadBtnSelector);
 
   try {
-    const res = await fetch(`/api/video_upload/${videoID}`, {
-      method: "POST",
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem("token")}`,
+    const res = await fetch(
+      `/api/video_upload/${videoID}`,
+      {
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('token')}`,
+        },
+        body: formData,
       },
-      body: formData,
-    });
+    );
     if (!res.ok) {
       const data = await res.json();
-      throw new Error(`Failed to upload video file. Error: ${data.error}`);
+      throw new Error(
+        `Failed to upload video file. Error: ${data.error}`,
+      );
     }
 
-    console.log("Video uploaded!");
+    console.log('Video uploaded!');
     await getVideo(videoID);
   } catch (error) {
     alert(`Error: ${error.message}`);
@@ -192,22 +223,24 @@ const videoStateHandler = createVideoStateHandler();
 
 async function getVideos() {
   try {
-    const res = await fetch("/api/videos", {
-      method: "GET",
+    const res = await fetch('/api/videos', {
+      method: 'GET',
       headers: {
-        Authorization: `Bearer ${localStorage.getItem("token")}`,
+        Authorization: `Bearer ${localStorage.getItem('token')}`,
       },
     });
     if (!res.ok) {
       const data = await res.json();
-      throw new Error(`Failed to get videos. Error: ${data.error}`);
+      throw new Error(
+        `Failed to get videos. Error: ${data.error}`,
+      );
     }
 
     const videos = await res.json();
-    const videoList = document.getElementById("video-list");
-    videoList.innerHTML = "";
+    const videoList = document.getElementById('video-list');
+    videoList.innerHTML = '';
     for (const video of videos) {
-      const listItem = document.createElement("li");
+      const listItem = document.createElement('li');
       listItem.textContent = video.title;
       listItem.onclick = () => videoStateHandler(video.id);
       videoList.appendChild(listItem);
@@ -225,8 +258,8 @@ function createVideoStateHandler() {
       currentVideoID = videoID;
 
       // Reset file input values
-      document.getElementById("thumbnail").value = "";
-      document.getElementById("video-file").value = "";
+      document.getElementById('thumbnail').value = '';
+      document.getElementById('video-file').value = '';
 
       await getVideo(videoID);
     }
@@ -236,13 +269,13 @@ function createVideoStateHandler() {
 async function getVideo(videoID) {
   try {
     const res = await fetch(`/api/videos/${videoID}`, {
-      method: "GET",
+      method: 'GET',
       headers: {
-        Authorization: `Bearer ${localStorage.getItem("token")}`,
+        Authorization: `Bearer ${localStorage.getItem('token')}`,
       },
     });
     if (!res.ok) {
-      throw new Error("Failed to get video.");
+      throw new Error('Failed to get video.');
     }
 
     const video = await res.json();
@@ -256,25 +289,33 @@ let currentVideo = null;
 
 function viewVideo(video) {
   currentVideo = video;
-  document.getElementById("video-display").style.display = "block";
-  document.getElementById("video-title-display").textContent = video.title;
-  document.getElementById("video-description-display").textContent =
-    video.description;
+  document.getElementById('video-display').style.display =
+    'block';
+  document.getElementById(
+    'video-title-display',
+  ).textContent = video.title;
+  document.getElementById(
+    'video-description-display',
+  ).textContent = video.description;
 
-  const thumbnailImg = document.getElementById("thumbnail-image");
+  const thumbnailImg = document.getElementById(
+    'thumbnail-image',
+  );
   if (!video.thumbnailURL) {
-    thumbnailImg.style.display = "none";
+    thumbnailImg.style.display = 'none';
   } else {
-    thumbnailImg.style.display = "block";
-    thumbnailImg.src = video.thumbnailURL;
+    thumbnailImg.style.display = 'block';
+    const TIME = new Date();
+    thumbnailImg.src = `${video.thumbnailURL}?v=${TIME.getTime()}`;
   }
 
-  const videoPlayer = document.getElementById("video-player");
+  const videoPlayer =
+    document.getElementById('video-player');
   if (videoPlayer) {
     if (!video.videoURL) {
-      videoPlayer.style.display = "none";
+      videoPlayer.style.display = 'none';
     } else {
-      videoPlayer.style.display = "block";
+      videoPlayer.style.display = 'block';
       videoPlayer.src = video.videoURL;
       videoPlayer.load();
     }
@@ -283,41 +324,51 @@ function viewVideo(video) {
 
 async function deleteVideo() {
   if (!currentVideo) {
-    alert("No video selected for deletion.");
+    alert('No video selected for deletion.');
     return;
   }
 
   try {
-    const res = await fetch(`/api/videos/${currentVideo.id}`, {
-      method: "DELETE",
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem("token")}`,
+    const res = await fetch(
+      `/api/videos/${currentVideo.id}`,
+      {
+        method: 'DELETE',
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('token')}`,
+        },
       },
-    });
+    );
     if (!res.ok) {
-      throw new Error("Failed to delete video.");
+      throw new Error('Failed to delete video.');
     }
-    alert("Video deleted successfully.");
-    document.getElementById("video-display").style.display = "none";
+    alert('Video deleted successfully.');
+    document.getElementById('video-display').style.display =
+      'none';
     await getVideos();
   } catch (error) {
     alert(`Error: ${error.message}`);
   }
 }
 
-document.getElementById("signup-button").addEventListener("click", signup);
-document.getElementById("logout-button").addEventListener("click", logout);
 document
-  .getElementById("thumbnail-upload-form")
-  .addEventListener("submit", async (event) => {
+  .getElementById('signup-button')
+  .addEventListener('click', signup);
+document
+  .getElementById('logout-button')
+  .addEventListener('click', logout);
+document
+  .getElementById('thumbnail-upload-form')
+  .addEventListener('submit', async (event) => {
     event.preventDefault();
     await uploadThumbnail(currentVideo?.id);
   });
 
 document
-  .getElementById("video-file-upload-form")
-  .addEventListener("submit", async (event) => {
+  .getElementById('video-file-upload-form')
+  .addEventListener('submit', async (event) => {
     event.preventDefault();
     await uploadVideoFile(currentVideo?.id);
   });
-document.getElementById("delete-video").addEventListener("click", deleteVideo);
+document
+  .getElementById('delete-video')
+  .addEventListener('click', deleteVideo);
