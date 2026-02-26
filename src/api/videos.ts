a0871f7946809c6fe86431e117bfc5aab1d9c74a
tkdlrs <tkd.lsanchez@gmail.com>
2026-02-25 18:36:43 -0700

    Completes 'Chapter 4: Object Storage | Lesson 3: Dynamic Path' of the 'Learn File Servers...in TypeScript' course on Boot.dev.

--- src/api/videos.ts
+++ src/api/videos.ts
@@ -46,7 +46,9 @@ export async function handlerUploadVideo(cfg: ApiConfig, req: BunRequest) {
   const tempFilePath = path.join("/tmp", `${videoId}.mp4`);
   await Bun.write(tempFilePath, file);
   //
-  let key = `${videoId}.mp4`;
+  const aspectRatio = await getVideoAspectRatio(tempFilePath);
+  //
+  let key = `${aspectRatio}/${videoId}.mp4`;
   await uploadVideoToS3(cfg, key, tempFilePath, "video/mp4");
   //
   const videoURL = `https://${cfg.s3Bucket}.s3.${cfg.s3Region}.amazonaws.com/${key}`;
@@ -56,4 +58,44 @@ export async function handlerUploadVideo(cfg: ApiConfig, req: BunRequest) {
   await Promise.all([rm(tempFilePath, { force: true })]);
   //
   return respondWithJSON(200, video);
+}
+//
+export async function getVideoAspectRatio(filePath: string) {
+  const process = Bun.spawn(
+    [
+      "ffprobe",
+      "-v",
+      "error",
+      "-select_streams",
+      "v:0",
+      "-show_entries",
+      "stream=width,height",
+      "-of",
+      "json",
+      filePath
+    ], {
+    stdout: "pipe",
+    stderr: "pipe"
+  });
+  //
+  const outputText = await new Response(process.stdout).text();
+  const errorText = await new Response(process.stderr).text();
+  //
+  const exitCode = await process.exited;
+  if (exitCode !== 0) {
+    throw new Error(`ffprobe error: ${errorText}`);
+  }
+  //
+  const output = JSON.parse(outputText);
+  if (!output.streams || output.streams.length === 0) {
+    throw new Error("No video streams found");
+  }
+  //
+  const { width, height } = output.streams[0];
+  return width === Math.floor(16 * (height / 9))
+    ? "landscape"
+    : height === Math.floor(16 * (width / 9))
+      ? "portrait"
+      : "other";
+  //
 }
